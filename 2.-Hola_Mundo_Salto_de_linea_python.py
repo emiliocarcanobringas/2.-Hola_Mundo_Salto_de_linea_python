bfc57d18d56efd2083d5704fb8cd007baf88186f
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:32:29 -0600

    Add files via upload

---
+++ 2.-Hola_Mundo_Salto_de_linea_python.py
@@ -0,0 +1,7 @@
+'''
+Este programa muestra como inprimir en pantalla información y como realizar
+un salto de línea con diagonal
+'''
+print("Mi nombre es Emilio Carcaño Bringas\nEstoy orgulloso de pertenecer a Uninaciones y estudiar en la UNAM")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
